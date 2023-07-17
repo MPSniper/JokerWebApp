6f0d77810ed7162d0ef7d5bec5cdc797215aea10
Mohammad Pourbarfeh <40278506+MPSniper@users.noreply.github.com>
2023-07-17 15:31:35 +0330

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokerWebApp.Models;
+
+namespace JokerWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokerWebApp.Models.Joke>? Joke { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace JokerWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
